555833c8167ff37f53cc26589ebd803e0ffa86ac
Christian Sachs <c.sachs@fz-juelich.de>
2021-05-19 12:38:00 +0200

    added more metadata and extended project configurations

--- junn-predict/junn_predict/__init__.py
+++ junn-predict/junn_predict/__init__.py
@@ -1,2 +1,13 @@
 """ JUNN, the Jülich U-Net Neural Network Segmentation Toolkit - Prediction Subset """
+
+__project__ = 'JUNN'
+
 __version__ = '1.0.0'
+
+__author__ = 'Christian C. Sachs'
+
+__copyright__ = '2021, Christian C. Sachs, Forschungszentrum Jülich GmbH'
+
+__citation__ = ''
+
+__banner__ = ''
