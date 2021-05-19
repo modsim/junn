2121d2a12d6439546eec01173a489513356cb7a3
Christian Sachs <c.sachs@fz-juelich.de>
2021-05-19 12:44:29 +0200

    suppressed warnings for putatively incompatible TensorFlow Addons

--- junn-predict/junn_predict/common/tensorflow_addons.py
+++ junn-predict/junn_predict/common/tensorflow_addons.py
@@ -1,16 +1,22 @@
+import warnings
+
+
 def try_load_tensorflow_addons():
     try:
-        import tensorflow_addons.activations as tfa_activations
-        from tensorflow.keras.utils import get_custom_objects
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
 
-        # bring TensorFlow Addons activations into 'normal' namespace
-        for activation in dir(tfa_activations):
-            decorated = 'Addons>%s' % (activation,)
-            if decorated in get_custom_objects():
-                get_custom_objects()[activation] = get_custom_objects()[decorated]
+            import tensorflow_addons.activations as tfa_activations
+            from tensorflow.keras.utils import get_custom_objects
 
-        from tensorflow_addons.register import register_all
+            # bring TensorFlow Addons activations into 'normal' namespace
+            for activation in dir(tfa_activations):
+                decorated = 'Addons>%s' % (activation,)
+                if decorated in get_custom_objects():
+                    get_custom_objects()[activation] = get_custom_objects()[decorated]
 
-        register_all()
+            from tensorflow_addons.register import register_all
+
+            register_all()
     except ImportError:
         pass
